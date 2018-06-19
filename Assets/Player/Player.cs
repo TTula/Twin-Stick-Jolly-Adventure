c0367ee33a896e1706e7fa112c68893f189bfe3b
TTula <TTula@users.noreply.github.com>
2018-06-19 15:26:07 +0200

    Adding RollerBall
    Added RollerBall to the Game

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+
+	
+	void Update () {
+		if (CrossPlatformInputManager.GetAxis("Vertical") == -1f)
+        {
+            Debug.Log("Down");
+        }
+        if (CrossPlatformInputManager.GetAxis("Vertical") == 1f)
+        {
+            Debug.Log("Up");
+        }
+        if (CrossPlatformInputManager.GetAxis("Horizontal") == -1f)
+        {
+            Debug.Log("Left");
+        }
+        if (CrossPlatformInputManager.GetAxis("Horizontal") == 1f)
+        {
+            Debug.Log("Right");
+        }
+    }
+}
